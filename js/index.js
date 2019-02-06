8fbb3d0eaab6aa1be939c2bde0eda4d07f331bcb
Mykol Benning <msmykolbenning@gmail.com>
2019-02-06 11:50:58 -0500

    added green nav

--- js/index.js
+++ js/index.js
@@ -55,6 +55,10 @@ nav[3].textContent = siteContent['nav']['nav-item-4']
 nav[4].textContent = siteContent['nav']['nav-item-5']
 nav[5].textContent = siteContent['nav']['nav-item-6']
 
+nav.forEach((a, cb) => {
+  a.style.color = "green";
+})
+
 //cta//
 const ctaH1 = document.querySelector('.cta h1');										
 ctaH1.textContent= siteContent.cta['h1']
